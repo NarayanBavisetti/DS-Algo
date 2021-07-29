67ac620487a0d4de26c12f2af31be6f4fcfbbf9e
narayan <narayan3119@gmail.com>
2021-07-29 18:38:40 +0530

    learnt character array

---
+++ character Array/characterarry.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+// char c[] = {'a','t','h','f'};
+// cout<<sizeof(c)<<endl;
+
+// char d[] = "hello";
+// cout<<sizeof(d);
+
+// char e;
+// cin>>e;
+// cout<<e;
+
+// char e[100];
+// cin>>e;
+// cout<<e;
+
+// char t[100];
+// cin.getline(t,100);
+// cout<<t;
+
+char paragaraph[1000];
+cin.getline(paragaraph,1000,'.');
+cout<<paragaraph;
+}
